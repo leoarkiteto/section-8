4db0c44b55c3ad2b434f366c367c6ceef6f9c8d4
Leo Franca <leoarkiteto@gmail.com>
2021-11-22 15:53:59 +0000

    Adding a re-usable "Button" Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import styles from './AddUser.module.css';
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 
 const AddUser = () => {
   const addUserHandler = event => {
@@ -9,10 +10,10 @@ const AddUser = () => {
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input type='text' id='username' />
-        <label htmlFor='age'>Age (Years)</label> <input type='text' id='age' />
-        <button type='submit'>Add User</button>
+        <label htmlFor='username'>Username</label>{' '}
+        <input type='text' id='username' />{' '}
+        <label htmlFor='age'>Age (Years)</label>
+        <input type='text' id='age' /> <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
